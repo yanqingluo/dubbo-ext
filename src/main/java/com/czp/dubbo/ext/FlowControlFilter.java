37028990dd6bc23b0eb883c709a77d19999b5522
coder_czp <coder_czp@126.com>
2016-01-09 13:06:55 +0800

    重构代码,添加日志打印

--- src/main/java/com/czp/dubbo/ext/FlowControlFilter.java
+++ src/main/java/com/czp/dubbo/ext/FlowControlFilter.java
@@ -1,4 +1,4 @@
-package com.czp.opensource;
+package com.czp.dubbo.ext;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -18,8 +18,8 @@ import com.alibaba.dubbo.rpc.RpcException;
  * 
  * <pre>
  *   以下任一场景将会将窗口调小SLIDE_RATIO:<br>
- *      1.失败率大于设定的值 FAILD_PERCENT_THRESHOLD
- *              ServerStatus.failCount/requestCount>FAILD_PERCENT_THRESHOLD
+ *      1.失败率大于设定的值 FAILD_RATIO_THRESHOLD
+ *              ServerStatus.failCount/requestCount>FAILD_RATIO_THRESHOLD
  *      2.一段时间内慢响应次数大于SLOW_RESPONSE_THRESHOLD 
  *              ServerStatus.slowReqCount>SLOW_RESPONSE_THRESHOLD
  *    如果一个统计周期内既没有失败也没有慢响应,则将窗口调大SLIDE_RATIO
@@ -34,7 +34,7 @@ public class FlowControlFilter implements Filter {
 	/** 放在同一个文件,方便理解 */
 	private static class ServerStatus {
 
-		/** 失败或 慢响应开始统计的时间,用于定时清除计数器 */
+		/** 失败或 慢响应开始统计的时间,用于定时清除计数 */
 		AtomicLong recordStart = new AtomicLong();
 
 		/** 总失败次数 */
@@ -43,18 +43,26 @@ public class FlowControlFilter implements Filter {
 		/** 总请求次数 */
 		AtomicInteger requestCount = new AtomicInteger();
 
-		/** 慢响应次数 ,该值会定时清除 */
+		/** 慢响应次数 */
 		AtomicInteger slowRespCount = new AtomicInteger();
 
 		/** 周期内最大请求数,会随后端服务状态调整 */
 		AtomicInteger maxRequest = new AtomicInteger(MIN_REQUEST);
 
-		void resetCounter() {
+		public void resetCounter() {
 			failCount.set(0);
-			requestCount.set(0);
+			requestCount.set(1);
 			slowRespCount.set(0);
-			maxRequest.set(MIN_REQUEST);
 		}
+
+		@Override
+		public String toString() {
+			return "ServerStatus [recordStart=" + recordStart + ", failCount="
+					+ failCount + ", requestCount=" + requestCount
+					+ ", slowRespCount=" + slowRespCount + ", maxRequest="
+					+ maxRequest + "]";
+		}
+
 	}
 
 	/** 滑动窗口的最大值 */
@@ -63,19 +71,19 @@ public class FlowControlFilter implements Filter {
 	/** 滑动窗口的最小值 */
 	private static final int MIN_REQUEST = 1000;
 
-	/** 滑动比例,每次增长或减缓按此比例进行 */
+	/** 滑动比例,每次增长或降低按此比例进行 */
 	private static final float SLIDE_RATIO = 0.3f;
 
-	/** 清除计数的时间周期,单位:毫秒 */
+	/** 计数统计周期,单位:毫秒 */
 	private static final int CLEAR_PEROID = 5 * 1000;
 
 	/** 慢响应次数超过该值时下调滑动窗口 */
 	private static final int SLOW_RESPONSE_THRESHOLD = 5;
 
-	/** 失败比例阈值,下调滑动窗口 */
+	/** 失败比例阈值,失败率大于此值时下调滑动窗口 */
 	private static final float FAILD_RATIO_THRESHOLD = 0.3f;
 
-	/** 每次响应耗时大于该值时会统计,单位:毫秒 */
+	/** 每次响应耗时大于该值时累加慢响应,单位:毫秒 */
 	private static final int RECORD_RESPONSE_THRESHOLD = 5 * 1000;
 
 	private static Logger logger = LoggerFactory.getLogger(FlowControlFilter.class);
@@ -85,9 +93,7 @@ public class FlowControlFilter implements Filter {
 
 	public Result invoke(Invoker<?> invoker, Invocation invocation)
 			throws RpcException {
-         
 		long curTime = System.currentTimeMillis();
-
 		/* 按IP:PORT统计,支持同一主机起多个服务 */
 		String address = invoker.getUrl().getAddress();
 		ServerStatus status = statusMap.get(address);
@@ -95,18 +101,22 @@ public class FlowControlFilter implements Filter {
 			statusMap.putIfAbsent(address, new ServerStatus());
 			status = statusMap.get(address);
 		}
-		int reqCount = status.requestCount.getAndIncrement();
 
+		if (logger.isDebugEnabled()) {
+			logger.debug(status.toString());
+		}
+
+		int reqCount = status.requestCount.getAndIncrement();
 		long recordStart = status.recordStart.get();
 		if (recordStart > 0 && (curTime > recordStart + CLEAR_PEROID)) {
 			/* 如果统计数据已过期,则重置计数器 */
 			status.resetCounter();
 		} else {
-			/* 达到当前窗口的极限,中断请求 */
+			/* 达到当前周期的极限,中断请求 */
 			int maxReq = status.maxRequest.get();
 			if (reqCount > maxReq) {
 				String info = createRequestInfo(address, invocation);
-				logger.info("interrupt:{},maxRequest:{}", info, maxReq);
+				logger.info("abort:{},maxRequest:{}", info, maxReq);
 				throw new RpcException(address.concat(" is busy"));
 			}
 		}
@@ -164,11 +174,13 @@ public class FlowControlFilter implements Filter {
 	 */
 	private void increaseMaxReq(ServerStatus status) {
 		int lastVal = status.maxRequest.get();
-		if (lastVal < MAX_REQUEST) {
-			int newVal = (int) (lastVal + lastVal * SLIDE_RATIO);
-			status.maxRequest.getAndSet(newVal);
-			logger.info("increase maxRequest from:{} to:{}", lastVal, newVal);
-		}
+		if (lastVal >= MAX_REQUEST)
+			return;
+		int newVal = (int) (lastVal + lastVal * SLIDE_RATIO);
+		if (newVal > MAX_REQUEST)
+			newVal = MAX_REQUEST;
+		status.maxRequest.set(newVal);
+		logger.info("increase maxRequest from:{} to:{}", lastVal, newVal);
 	}
 
 	/**
@@ -177,12 +189,14 @@ public class FlowControlFilter implements Filter {
 	 * @param status
 	 */
 	private void decreaseMaxReq(ServerStatus status) {
-		int lastVal = status.maxRequest.get();
-		if (lastVal > MIN_REQUEST) {
-			int newVal = (int) (lastVal - lastVal * SLIDE_RATIO);
-			status.maxRequest.getAndSet(newVal);
-			logger.info("decrease maxRequest from:{} to:{}", lastVal, newVal);
-		}
+		int oldVal = status.maxRequest.get();
+		if (oldVal <= MIN_REQUEST)
+			return;
+		int newVal = (int) (oldVal - oldVal * SLIDE_RATIO);
+		if (newVal < MIN_REQUEST)
+			newVal = MIN_REQUEST;
+		status.maxRequest.set(newVal);
+		logger.info("decrease maxRequest from:{} to:{}", oldVal, newVal);
 	}
 
 	/**
